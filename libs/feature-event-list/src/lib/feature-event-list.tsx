12c1c953371bac3c2fb1b51217a5d6ce105dd6ba
Vitalie Andries <vitalie.andries@gmail.com>
2025-02-14 15:02:36 +0200

    feat: add event list feature placeholder

--- libs/feature-event-list/src/lib/feature-event-list.tsx
+++
@@ -1,9 +0,0 @@
-export function FeatureEventList() {
-    return (
-        <div className={styles['container']}>
-            <h1>Welcome to FeatureEventList!</h1>
-        </div>
-    );
-}
-
-export default FeatureEventList;
