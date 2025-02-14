aff5f435de002d64959a0c1bed21f97ba889df3c
Vitalie Andries <vitalie.andries@gmail.com>
2025-02-14 12:39:28 +0200

    feat: add feature-ws-monitor library with initial implementation

---
+++ libs/feature-ws-monitor/src/lib/feature-ws-monitor.tsx
@@ -0,0 +1,9 @@
+export function FeatureWsMonitor() {
+    return (
+        <div className={styles['container']}>
+            <h1>Welcome to FeatureWsMonitor!</h1>
+        </div>
+    );
+}
+
+export default FeatureWsMonitor;
