85a8366ee0ffedfe159f7e3d653e0ca42db3dde4
Vitalie Andries <vitalie.andries@gmail.com>
2025-02-14 16:35:35 +0200

    chore: create betslip lib placeholder

---
+++ libs/feature-event-betslip/src/lib/feature-betslip.tsx
@@ -0,0 +1,9 @@
+export function FeatureBetslip() {
+    return (
+        <div className={styles['container']}>
+            <h1>Welcome to FeatureBetslip!</h1>
+        </div>
+    );
+}
+
+export default FeatureBetslip;
